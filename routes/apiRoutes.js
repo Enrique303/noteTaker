33493251fc360ecfd8e0eb350fbea83917fbc54e
Enrique Lopez <elop22@gmail.com>
2020-09-08 22:10:28 -0600

    added uuid

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,6 +1,7 @@
 const notes = require('../db/db.json');
 const fs = require('fs');
 const path = require('path');
+const uuid = require('uuid');
 
 module.exports = function(app){
   app.get('/api/notes', function(req, res){
@@ -8,17 +9,17 @@ module.exports = function(app){
   });
   app.post('/api/notes', function(req,res){
     const newNote = req.body;
+    newNote.id = uuid
     notes.push(newNote);
     writeFile(notes)
     res.json(newNote);
   });
   app.delete('/api/notes/:id', function(req,res){
-    fs.readFile(notes, function(){
       const deleteNote = req.param.id;
       if(notes.id === deleteNote){
         notes.splice()
       }
-    })
+    
   })
   function writeFile(){
     fs.writeFileSync('db/db.json', JSON.stringify(notes))
